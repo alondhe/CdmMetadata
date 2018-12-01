454703ef831febc20547608a2ac819d6d385bce1
Ajit Londhe <alondhe2@its.jnj.com>
2018-12-01 02:23:16 -0800

    fixed crud operations in manage tab

--- inst/shinyApps/management/server.R
+++ inst/shinyApps/management/server.R
@@ -4,8 +4,24 @@ library(magrittr)
 library(tidyr)
 
 shinyServer(function(input, output, session) {
+
   
-  Agents <- eventReactive(input$addAgent, {
+  agentTextInputs <- c("agentFirstName",
+                       "agentLastName",
+                       "agentSuffix",
+                       "agentAlgorithm",
+                       "agentDescription")
+  
+  agentChoices <- c("Human" = "hum", "Algorithm" = "alg")
+    
+  .clearTextInputs <- function(textInputNames) {
+    for (t in textInputNames) {
+      updateTextInput(session, t, value = "")
+    }  
+    updateActionButton(session = session, inputId = "btnSubmitAgent", label = "Add New Agent")
+  }
+  
+  .getAgents <- function() {
     connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = Sys.getenv("dbms"), 
                                                                     user = Sys.getenv("user"),
                                                                     password = Sys.getenv("password"), 
@@ -13,19 +29,44 @@ shinyServer(function(input, output, session) {
                                                                     port = Sys.getenv("port"))
     
     connection <- DatabaseConnector::connect(connectionDetails = connectionDetails)
-    on.exit(DatabaseConnector::disconnect(connection = connection))
-    
     resultsDatabaseSchema <- Sys.getenv("resultsDatabaseSchema")
     
-    sql <- SqlRender::renderSql("select * from @resultsDatabaseSchema.meta_agent;",
+    sql <- SqlRender::renderSql("select * from @resultsDatabaseSchema.meta_agent
+                                order by meta_agent_id;",
                                 resultsDatabaseSchema = resultsDatabaseSchema)$sql
     sql <- SqlRender::translateSql(sql = sql, targetDialect = connectionDetails$dbms)$sql
     df <- DatabaseConnector::querySql(connection = connection, sql = sql)
+    DatabaseConnector::disconnect(connection = connection)  
+    
     df
-  }, ignoreNULL = FALSE)
+  }
+  
+  output$dtAgent <- renderDataTable({
+    input$btnSubmitAgent
+    input$btnDeleteAgent
+    df <- .getAgents()
+    
+    options <- list(pageLength = 10000,
+                    searching = TRUE,
+                    lengthChange = FALSE,
+                    ordering = TRUE,
+                    paging = FALSE,
+                    scrollY = '75vh')
+    selection <- list(mode = "single", target = "row")
+
+    table <- datatable(df,
+                       options = options,
+                       selection = "single",
+                       rownames = FALSE,
+                       class = "stripe nowrap compact", extensions = c("Responsive"))
+    table <- formatStyle(table = table,
+                         columns = ncol(df),
+                         target = "row")
+    table
+  }, server = FALSE)
   
   
-  getMaxId <- function(tableName, fieldName) {
+  .getMaxId <- function(tableName, fieldName) {
     connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = Sys.getenv("dbms"), 
                                                                     user = Sys.getenv("user"),
                                                                     password = Sys.getenv("password"), 
@@ -51,7 +92,7 @@ shinyServer(function(input, output, session) {
     }
   }
   
-  observeEvent(input$addAgent, {
+  .addAgent <- function() {
     resultsDatabaseSchema <- Sys.getenv("resultsDatabaseSchema")
     connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = Sys.getenv("dbms"), 
                                                                     user = Sys.getenv("user"),
@@ -60,11 +101,10 @@ shinyServer(function(input, output, session) {
                                                                     port = Sys.getenv("port"))
     
     connection <- DatabaseConnector::connect(connectionDetails = connectionDetails)
-    on.exit(DatabaseConnector::disconnect(connection = connection))
     
     df <- data.frame(
-      meta_agent_id = getMaxId(tableName = "meta_agent", fieldName = "meta_agent_id") + 1,
-      meta_agent_concept_id = 1000,
+      meta_agent_id = .getMaxId(tableName = "meta_agent", fieldName = "meta_agent_id") + 1,
+      meta_agent_concept_id = ifelse(input$agentConcept == "hum", 1000, 2000),
       agent_first_name = input$agentFirstName,
       agent_last_name = input$agentLastName,
       agent_suffix = input$agentSuffix,
@@ -76,22 +116,102 @@ shinyServer(function(input, output, session) {
                                    tableName = sprintf("%s.meta_agent", resultsDatabaseSchema), 
                                    data = df, 
                                    dropTableIfExists = FALSE, createTable = FALSE, useMppBulkLoad = FALSE) 
+    DatabaseConnector::disconnect(connection = connection)
     
     showNotification(sprintf("New agent added"))
     
-    choices <- c("Load Existing" = "load", "Add New" = "add")
-    updateSelectInput(session, "loadOrAddAgent",
-                      label = "Load Existing or Add New Agent", 
-                      choices = choices,
-                      selected = head(choices, 1)
-    )
+    .clearTextInputs(textInputNames = agentTextInputs)
+  }
+  
+  .updateAgent <- function() {
     
-    input$loadOrAddAgent
+    resultsDatabaseSchema <- Sys.getenv("resultsDatabaseSchema")
+    connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = Sys.getenv("dbms"), 
+                                                                    user = Sys.getenv("user"),
+                                                                    password = Sys.getenv("password"), 
+                                                                    server = Sys.getenv("server"), 
+                                                                    port = Sys.getenv("port"))
+    
+    connection <- DatabaseConnector::connect(connectionDetails = connectionDetails)
+    
+    row_count <- input$dtAgent_rows_selected
+    
+    sql <- SqlRender::loadRenderTranslateSql(sqlFilename = "management/updateAgent.sql", 
+                                             packageName = "CdmMetadata", 
+                                             dbms = connectionDetails$dbms,
+                                             resultsDatabaseSchema = resultsDatabaseSchema,
+                                             agentFirstName = input$agentFirstName,
+                                             agentLastName = input$agentLastName,
+                                             agentSuffix = input$agentSuffix,
+                                             agentAlgorithm = input$agentAlgorithm,
+                                             agentDescription = input$agentDescription,
+                                             metaAgentId = .getAgents()[row_count, ]$META_AGENT_ID)
+
+    
+    DatabaseConnector::executeSql(connection = connection, sql = sql)
+    DatabaseConnector::disconnect(connection = connection)
+    
+    showNotification(sprintf("Agent updated"))
+  }
+  
+  .deleteAgent <- function() {
+    resultsDatabaseSchema <- Sys.getenv("resultsDatabaseSchema")
+    connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = Sys.getenv("dbms"), 
+                                                                    user = Sys.getenv("user"),
+                                                                    password = Sys.getenv("password"), 
+                                                                    server = Sys.getenv("server"), 
+                                                                    port = Sys.getenv("port"))
+    
+    connection <- DatabaseConnector::connect(connectionDetails = connectionDetails)
+    
+    row_count <- input$dtAgent_rows_selected
+    metaAgentId <- .getAgents()[row_count, ]$META_AGENT_ID
+    
+    sql <- SqlRender::renderSql("delete from @resultsDatabaseSchema.meta_agent where meta_agent_id = '@metaAgentId';",
+                                resultsDatabaseSchema = resultsDatabaseSchema,
+                                metaAgentId = metaAgentId)$sql
+    
+    sql <- SqlRender::translateSql(sql = sql, targetDialect = connectionDetails$dbms)$sql
+    DatabaseConnector::executeSql(connection = connection, sql = sql)
+    DatabaseConnector::disconnect(connection = connection)
+    
+    showNotification(sprintf("Agent deleted"))
+  }
+  
+  observeEvent(input$btnDeleteAgent, handlerExpr = {
+    .deleteAgent()
+    .clearTextInputs(agentTextInputs)
+  }, priority = 1)
+  
+  observeEvent(eventExpr = input$btnClearAgent, handlerExpr = {
+    .clearTextInputs(textInputNames = agentTextInputs)
   })
   
-  output$dtAgent <- renderDataTable({
-    Agents()
+  observeEvent(eventExpr = input$dtAgent_rows_selected, handlerExpr = {
+    updateActionButton(session = session, inputId = "btnSubmitAgent", label = "Update Agent")
+    row_count <- input$dtAgent_rows_selected
+
+    if (.getAgents()[row_count,]$META_AGENT_CONCEPT_ID == 1000) {
+      updateSelectInput(session = session, inputId = "agentConcept", selected = agentChoices[1])
+    } else {
+      updateSelectInput(session = session, inputId = "agentConcept", selected = agentChoices[2])
+    }
+    
+    for (t in agentTextInputs) {
+      updateTextInput(session = session,
+                      inputId = t,
+                      value = .getAgents()[row_count,][toupper(SqlRender::camelCaseToSnakeCase(t))][[1]])
+    }
   })
+  
+  observeEvent(eventExpr = input$btnSubmitAgent, handlerExpr = {
+    agentSelected <- !is.null(input$dtAgent_rows_selected)
+    if (agentSelected) {
+      .updateAgent()
+    } else {
+      .addAgent()
+    }
+  }, priority = 1)
   
   EntityActivities <- reactive({
     connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = Sys.getenv("dbms"), 
@@ -128,4 +248,5 @@ shinyServer(function(input, output, session) {
     df
   })
  
+  
 })
--- inst/shinyApps/management/ui.R
+++ inst/shinyApps/management/ui.R
@@ -4,87 +4,63 @@ library(DT)
 ui <- fluidPage(
   
   # App title ----
-  titlePanel(title = div(img(src = "ohdsi_logo.png"), strong(sprintf("CDM Metadata Management: %s", Sys.getenv("sourceName")))), 
-             windowTitle = "CDM Metadata Management"),
+  titlePanel(title = div(img(src = "ohdsi_logo.png"), strong(sprintf("CDM Metadata: %s", Sys.getenv("sourceName")))), 
+             windowTitle = "CDM Metadata"),
   
-  # Sidebar layout with input and output definitions ----
-  sidebarLayout(
-    
-    # Sidebar panel for inputs ----
-    sidebarPanel(
-      
-      tabsetPanel(type = "pills",
-                  tabPanel("Identify Agent", 
-                           h3("Agent Information"),
-                           helpText("Enter information about a metadata creator (can be a human or an algorithm)"),
-                           
-                           selectInput(inputId = "loadOrAddAgent", label = "Load Existing or Add New Agent", 
-                                       choices = c("Load Existing" = "load", "Add New" = "add")),
-                           
-                           conditionalPanel(condition = "input.loadOrAddAgent == 'load'",
-                                            DT::dataTableOutput("dtAgent")
+  tabsetPanel(type = "pills",
+              tabPanel("Manage Metadata",
+                       tabsetPanel(type = "tabs",
+                                   tabPanel("Agents", 
+                                            h3("Agent Information"),
+                                            helpText("Manage information about a metadata creator (can be a human or an algorithm)"),
+                                            sidebarLayout(
+                                              sidebarPanel(
+                                                selectInput(inputId = "agentConcept", label = "Agent Type", choices = c("Human" = "hum", "Algorithm" = "alg")),
+                                                conditionalPanel(condition = "input.agentConcept == 'hum'",
+                                                                 textInput(inputId = "agentFirstName", label = "First Name"),
+                                                                 textInput(inputId = "agentLastName", label = "Last Name"),
+                                                                 textInput(inputId = "agentSuffix", label = "Suffix")
+                                                                 ),
+                                                conditionalPanel(condition = "input.agentConcept == 'alg'",
+                                                                 textInput(inputId = "agentAlgorithm", label = "Algorithm Name"),
+                                                                 textAreaInput(inputId = "agentDescription", label = "Description", 
+                                                                               rows = 4, resize = "none")
+                                                                 ),
+                                                actionButton(inputId = "btnClearAgent", label = "Clear Selected Agent", icon = icon("user-edit")),
+                                                actionButton(inputId = "btnSubmitAgent", label = "Add New Agent"),
+                                                actionButton(inputId = "btnDeleteAgent", label = "Delete Selected Agent", icon = icon("user-minus"))
+                                                ),
+                                              mainPanel(DT::dataTableOutput("dtAgent"))
+                                              )
                                             ),
-                           
-                           conditionalPanel(condition = "input.loadOrAddAgent == 'add'",
-                             
-                             selectInput(inputId = "agentConcept", label = "Agent Type", choices = c("Human" = "hum", "Algorithm" = "alg")),
-                        
-                             conditionalPanel(condition = "input.agentConcept == 'hum'",
-                             
-                               textInput(inputId = "agentFirstName", label = "First Name"),
-                               textInput(inputId = "agentLastName", label = "Last Name"),
-                               textInput(inputId = "agentSuffix", label = "Suffix")
-                             ),
-                             conditionalPanel(condition = "input.agentConcept == 'alg'",
-                               textInput(inputId = "agentAlgorithm", label = "Algorithm Name"),
-                               textAreaInput(inputId = "agentDescription", label = "Description", 
-                                             rows = 4, resize = "none")
-                             ),
-                             actionButton(inputId = "addAgent", label = "Add New Agent")
-                           )
-                           ),
-                  tabPanel("Add New Metadata", 
-                           h3("Add New Metadata"),
-                           helpText("Enter the new metadata information. Metadata consists of an entity (the artifact of the CDM database that you wish to tag with metadata)
-                                    and an activity (the phenomena you wish to describe about the entity)."),
-                           
-                           selectInput(inputId = "entityConceptId", 
-                                       label = "Entity Concept Id", choices = c("None" = 0)),
-                           textInput(inputId = "entityAsString",
-                                     label = "Entity As String"),
-                           textInput(inputId = "entityIdentifier",
-                                     label = "Entity Identifer"),
-                   
-                           selectInput(inputId = "activityConceptId", 
-                                       label = "Activity Concept Id", choices = c("None" = 0)),
-                           selectInput(inputId = "activityTypeConceptId", 
-                                       label = "Activity Type Concept Id", choices = c("None" = 0)),
-                           textInput(inputId = "activityAsString",
-                                     label = "Activity As String"),
-                           
-                           #actionButton(inputId = "addAnnotation", label = "Add Annotation(s)"),
-                           actionButton(inputId = "addValue", label = "Add Associated Values")
-                  ),
-                  tabPanel("Add New Annotation", 
-                           h3("Add New Annotation"),
-                           helpText("Enter new annotation information for an existing entity/activity record.")
-                           
-                           
-                  ),
-                  tabPanel("Add Associated Values", 
-                           h3("Add Associated Values"),
-                           helpText("Enter any associated value with the new metadata or annotation records. You can insert multiple values."),
-                           numericInput(inputId = "valueOrdinal", value = 1, label = "Ordinal"),
-                           selectInput(inputId = "valueConceptId",
-                                       label = "Value Concept Id", choices = c("None" = 0))
-                  )
-      )
-    ),
-    
-    # Main panel for displaying outputs ----
-    mainPanel(
-
-      
-    )
+                                   tabPanel("Entities and Activities", 
+                                            h3("Entities and Activities"), 
+                                            helpText("Enter the new metadata information. Metadata consists of an entity (the artifact of the CDM database that you wish to tag with metadata) 
+                                                     and an activity (the phenomena you wish to describe about the entity)."),
+                                            selectInput(inputId = "entityConceptId", label = "Entity Concept Id", choices = c("None" = 0)),
+                                            textInput(inputId = "entityAsString", label = "Entity As String"), textInput(inputId = "entityIdentifier", label = "Entity Identifer"),
+                                            selectInput(inputId = "activityConceptId", label = "Activity Concept Id", choices = c("None" = 0)), 
+                                            selectInput(inputId = "activityTypeConceptId",
+                                                        label = "Activity Type Concept Id", choices = c("None" = 0)),
+                                            textInput(inputId = "activityAsString",
+                                                      label = "Activity As String"),
+                                            dateRangeInput(inputId = "activityDates", label = "Activity Date Start and End"),
+                                            #actionButton(inputId = "addAnnotation", label = "Add Annotation(s)"),
+                                            actionButton(inputId = "addValue", label = "Add Associated Values")
+                                            ),
+                                   tabPanel("Annotations", 
+                                            h3("Annotations"),
+                                            helpText("Enter new annotation information for an existing entity/activity record.")
+                                            ),
+                                   tabPanel("Associated Values", 
+                                            h3("Associated Values"),
+                                            helpText("Enter any associated value with the new metadata or annotation records. You can insert multiple values."),
+                                            numericInput(inputId = "valueOrdinal", value = 1, label = "Ordinal"),
+                                            selectInput(inputId = "valueConceptId",
+                                                        label = "Value Concept Id", choices = c("None" = 0))
+                                            )
+                                   )
+                       ),
+              tabPanel("Explore Metadata")
+              )
   )
-)
